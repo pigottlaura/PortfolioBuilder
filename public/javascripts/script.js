8419b48cf93d2d2cad9ba5ba4e113023a04aea82
Laura Pigott <pigottlaura@gmail.com>
2016-04-16 20:26:59 +0100

    Checking CreateAccount form before allowing submit
    Checking that all fields in the form have a value supplied, and that the password and confirm password fields match, before allowing the form to be submitted

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,23 +1,21 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     console.log("Document ready");
     $(".tabs").tabs();
 
-    $("#createAccount input").change(function() {
-        
-        $("input[name='username'], input[name='portfolioURL']").val(function(){
+    $("#createAccount input").change(function () {
+
+        $("input[name='username'], input[name='portfolioURL']").val(function () {
             return $(this).val().toLowerCase().replace(/ /g, "");
         });
-        
-        var username = $("input[name='username']").val().toLowerCase();
-        var url = $("input[name='portfolioURL']").val().toLowerCase();
-        
-        if ($("input[name='portfolioURL']").val() == "") {
-            $("input[name='portfolioURL']").val(username.replace(/ /g, ""));
-        }
-        
-        $.post("/checkCredentialsAvailable", { requestedUsername: username, requestedPortfolioURL: url }, function(responseData) {
-            console.log(responseData);
 
+        var formUsername = $("input[name='username']").val().toLowerCase();
+        var formPortfolioUrl = $("input[name='portfolioURL']").val().toLowerCase();
+
+        if ($("input[name='portfolioURL']").val() == "") {
+            $("input[name='portfolioURL']").val(formUsername.replace(/ /g, ""));
+        }
+
+        checkCredentialsAvailable(formUsername, formPortfolioUrl, function (responseData) {
             if (responseData.usernameAvailable) {
                 $("#usernameStatusIcon").attr("class", "glyphicon glyphicon-ok-circle");
             } else {
@@ -31,8 +29,59 @@ jQuery(document).ready(function($) {
                 console.log("Portfolio URL not available");
                 $("#portfolioURLStatusIcon").attr("class", "glyphicon glyphicon-ban-circle");
             }
-
-
-        }, "json");
+        });
     });
+
+    $("#createAccount").submit(function (event) {
+        // Creating a boolean which will be used to determine if this form should be allowed to 
+        // submit or not. This will only be set to true if all required form fields have a value
+        // supplied in them, and the password/confirmPassword field's values match
+        var allowSubmit = false;
+        var allRequiredFieldsSupplied = true;
+
+        var username = $(this).find("input[name='username']").val().toLowerCase();
+        var portfolioURL = $(this).find("input[name='portfolioURL']").val().toLowerCase();
+
+        // Checking all input fields within this form, to ensure that none are left empty.
+        // Excluding the submit input from this search, as this cannot take a user defined
+        // value.
+        $(this).find("input").not("[type='submit']").each(function (index, element) {
+            if ($(element).val() == "") {
+                allRequiredFieldsSupplied = false;
+                $(element).addClass("formWarning");
+            } else {
+                $(element).removeClass("formWarning");
+            }
+        });
+
+        if (allRequiredFieldsSupplied) {
+            console.log("All fields supplied");
+            if ($(this).find("input[name='password']").val() == $(this).find("input[name='confirmPassword']").val()) {
+                console.log("passwords match");
+                allowSubmit = true;
+            } else {
+                console.log("passwords don't match");
+            }
+        } else {
+            console.log("missing fields");
+        }
+
+        console.log("about to return from function");
+        return allowSubmit;
+    });
+
+    $("#changePortfolioURL").click(function () {
+        console.log("click");
+    });
+    
+    // Creating an asynchronous function to check if the credentials a user has supplied are available.
+    // Takes in three parametres. The requested username, requested url, and the callback function to which
+    // the response data should be returned when a response is received from the server
+    function checkCredentialsAvailable(username, url, cb) {
+        $.post("/checkCredentialsAvailable", { requestedUsername: username, requestedPortfolioURL: url }, function (responseData) {
+            
+            // Passing the response data back to the callback function
+            cb(responseData);
+        }, "json");
+    }
 });
--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var databaseModels = require("../custom_modules/databaseModels");
 var MediaItem = databaseModels.MediaItem;
+var User = databaseModels.User;
 
 /* GET admin listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   console.log("Admin - in admin section");
   res.render("admin", {
     title: "Admin Section",
@@ -17,7 +18,7 @@ router.get('/', function(req, res, next) {
 
 });
 
-router.post("/uploadMedia", function(req, res, next) {
+router.post("/uploadMedia", function (req, res, next) {
   console.log("Admin - file successfully uploaded");
   var newMediaItem = new MediaItem({
     file: req.files[0],
@@ -26,7 +27,7 @@ router.post("/uploadMedia", function(req, res, next) {
     filePath: "../" + req.files[0].path.split("public\\")[1],
     fileTitle: req.body.mediaItemTitle
   });
-  newMediaItem.save(function(err, newMediaItem) {
+  newMediaItem.save(function (err, newMediaItem) {
     if (err) {
       console.log("Admin - Could not save media item to database - " + err);
     } else {
@@ -35,5 +36,18 @@ router.post("/uploadMedia", function(req, res, next) {
     }
   });
 });
+
+/*
+router.post("/changePortfolioURL", function (req, res, next) {
+  console.log("Admin - requested portfolioURL to be changed");
+  User.update({ username: req.session.username }, { $set: { portfolioURL: req.body.requestedURL} }, function (err, user) {
+    if (err) {
+      console.log("Admin - Could not check if this username exists - " + err);
+    } else {
+      console.log("Admin - updated " + user.username + "'s portfolio URL to " + user.portfolioURL);
+    }
+  });
+});
+*/
 
 module.exports = router;
