6ae985ed6b69fdab02df478bd69632e442b99abd
Laura Pigott <pigottlaura@gmail.com>
2016-05-01 14:08:05 +0100

    Fix to figure sizing for objects

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -52,7 +52,7 @@ function resizeFigures() {
 
     // Resetting each figure's minHeight to it's initial value, so that when the resizeFigures() funciton
     // runs, it is not basing it's new height value on the current dimensions of the figures
-    $("figure").css("minHeight", "initial");
+    $("figure").css("minHeight", $("figure img").parent().height());
     $("figure video, figure object, .objectContainer").css("height", $("figure img").height());
 
     // Creating a temporary variable to store the largest height of the figures currently
